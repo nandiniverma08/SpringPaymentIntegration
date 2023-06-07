16dc5dd9b2a5f7aad2217e8a117b75097c8978b1
nandiniverma08 <vermanandini818@gmail.com>
2023-06-07 12:31:19 +0530

    first commit

---
+++ src/main/java/com/paymentDemo/spring/paypal/api/SpringPaypalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.paymentDemo.spring.paypal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalExampleApplication.class, args);
+	}
+
+}
